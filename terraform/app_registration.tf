850c74f61be9cc68a9aad9d99a920f5a71f45216
ivan-saji <ivansajimeleth@gmail.com>
2026-07-25 14:53:08 +0530

    working on secret

--- terraform/app_registration.tf
+++ terraform/app_registration.tf
@@ -7,13 +7,13 @@ resource "azuread_application" "app_storage_uploader" {
 # Service Principals
 
 resource "azuread_service_principal" "sp_storage_uploader" {
-    client_id = azuread_application.app_storage_uploader.application_id
+    client_id = azuread_application.app_storage_uploader.id
 }
 
 # Client Secret
 
 resource "azuread_application_password" "sp_storage_uploader_secret" {
-    application_id = azuread_application.app_storage_uploader.object_id
-    display_name          = "terraform-storage-uploader-secret"
-    end_date_relative     = "8760h" # 1 year
+    application_id = azuread_application.app_storage_uploader.id
+    display_name   = "terraform-storage-uploader-secret"
+    end_date_relative = "4320h"  # 180 days
 }
