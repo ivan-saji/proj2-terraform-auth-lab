5572e24a29481f6b723fc6d23cb0ee3038346a16
ivan-saji <ivansajimeleth@gmail.com>
2026-07-25 13:13:39 +0530

    Created service principal and secret

--- terraform/app_registration.tf
+++ terraform/app_registration.tf
@@ -1,6 +1,19 @@
 # Create you app registrations here
 
-resource "azuread_application" "storage_uploader" {
+resource "azuread_application" "app_storage_uploader" {
     display_name = "terraform-storage-uploader"
 }
 
+# Service Principals
+
+resource "azuread_service_principal" "sp_storage_uploader" {
+    client_id = azuread_application.app_storage_uploader.application_id
+}
+
+# Client Secret
+
+resource "azuread_application_password" "sp_storage_uploader_secret" {
+    application_id = azuread_application.app_storage_uploader.object_id
+    display_name          = "terraform-storage-uploader-secret"
+    end_date_relative     = "8760h" # 1 year
+}
